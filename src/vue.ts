3b6adb9d78709ad9b24674ea428568e2d79ef4eb
york yao <yaoao12306@outlook.com>
2017-06-02 15:21:57 +0800

    done local search

--- src/vue.ts
+++ src/vue.ts
@@ -15,11 +15,34 @@ class Select2 extends Vue {
     optionLabel = "";
     isOpen = false;
     focusoutTimer?: NodeJS.Timer;
+    searchText = "";
 
     get dropdownStyle() {
         return this.isOpen
             ? "select2-container select2-container--default select2-container--open"
             : "select2-container select2-container--default";
+    }
+
+    get filteredData() {
+        if (this.searchText) {
+            const result: common.Select2Data = [];
+            for (const groupOrOption of this.data) {
+                if ((groupOrOption as common.Select2Group).options) {
+                    if ((groupOrOption as common.Select2Group).options.some(group => this.containSearchText(group.label))) {
+                        const options = (groupOrOption as common.Select2Group).options.filter(group => this.containSearchText(group.label));
+                        result.push({
+                            label: groupOrOption.label,
+                            options,
+                        });
+                    }
+                } else if (this.containSearchText(groupOrOption.label)) {
+                    result.push(groupOrOption);
+                }
+            }
+            return result;
+        } else {
+            return this.data;
+        }
     }
 
     beforeMount() {
@@ -40,6 +63,9 @@ class Select2 extends Vue {
         }
     }
 
+    containSearchText(label: string) {
+        return this.searchText ? label.toLowerCase().indexOf(this.searchText.toLowerCase()) > -1 : true;
+    }
     getOptionStyle(value: string) {
         return value === this.hoveringValue
             ? "select2-results__option select2-results__option--highlighted"
@@ -63,6 +89,7 @@ class Select2 extends Vue {
     toggleOpenAndClose() {
         this.isOpen = !this.isOpen;
         if (this.isOpen) {
+            this.searchText = "";
             Vue.nextTick(() => {
                 const searchInput = this.$refs.searchInput as HTMLElement;
                 if (searchInput) {
