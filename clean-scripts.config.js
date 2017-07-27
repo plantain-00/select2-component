521567660c2c05e60f86cee81b52c108d8a738f4
york yao <yaoao12306@outlook.com>
2017-07-28 00:06:38 +0800

    clean scripts

---
+++ clean-scripts.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  build: [
+    `rimraf dist`,
+    `rimraf demo/**/index.bundle-*.js`,
+    `rimraf demo/*.bundle-*.css`,
+    `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
+    `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src`,
+    `tsc -p src`,
+    `tsc -p demo`,
+    `lessc src/select2.less > dist/select2.css`,
+    `cleancss -o dist/select2.min.css dist/select2.css`,
+    `cleancss -o demo/index.bundle.css dist/select2.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
+    `webpack --config demo/webpack.config.js`,
+    `rev-static --config demo/rev-static.config.js`
+  ],
+  lint: [
+    `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
+    `standard "**/*.config.js"`,
+    `stylelint "src/**/*.less"`
+  ],
+  test: [
+    'tsc -p spec',
+    'karma start spec/karma.config.js'
+  ],
+  fix: [
+    `standard --fix "**/*.config.js"`
+  ],
+  release: [
+    `clean-release`
+  ]
+}
