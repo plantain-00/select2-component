b91420aaca24a22240908f98e6afd9a47addb9f8
cvs <celian-veyssiere@harvest.fr>
2017-07-31 14:59:16 +0200

    Change Angular composent for use NgControl
    - change for use NgControl (for formControlName/ngModel) or not
    - add composent in Angular Module
    - a begin of Material style (with “material” argument)

--- src/angular.ts
+++ src/angular.ts
@@ -1,38 +1,48 @@
-import { Component, Input, Output, EventEmitter, ElementRef, ViewChild } from "@angular/core";
+import {
+    Component, Input, Output, EventEmitter, ElementRef, ViewChild, Optional, Self, ChangeDetectorRef
+} from "@angular/core";
+import {
+    FormGroupDirective, NgControl, NgForm, ControlValueAccessor
+} from '@angular/forms';
+
+import { Subject } from 'rxjs';
+
 import * as common from "./common";
 export * from "./common";
-import { angularTemplateHtml } from "./angular-variables";
+
+let nextUniqueId = 0;
 
 @Component({
-    selector: "select2",
-    template: angularTemplateHtml,
+    selector: 'select2',
+    styleUrls: ['./angulat.scss'],
+    templateUrl: './select2.html',
+    host: {
+        '[id]': 'id',
+        '[attr.aria-invalid]': '_isErrorState()',
+        '[class.select2-invalid]': '_isErrorState()',
+    }
 })
-export class Select2Component {
-    @Input()
-    data: common.Select2Data;
-    @Input()
-    value?: common.Select2UpdateValue;
-    @Input()
-    disabled?: boolean;
-    @Input()
-    minCountForSearch?: number;
-    @Input()
-    placeholder?: string;
-    @Input()
-    customSearchEnabled?: boolean;
-    @Input()
-    multiple?: boolean;
-    @Output()
-    update = new EventEmitter();
-    @Output()
-    open = new EventEmitter();
-    @Output()
-    search = new EventEmitter();
+export class Select2 implements ControlValueAccessor {
+
+    /** data of options & optiongrps */
+    @Input() data: common.Select2Data;
+    /** minimal data of show the search field */
+    @Input() minCountForSearch?: number;
+    @Input() placeholder?: string;
+    @Input() customSearchEnabled?: boolean;
+    @Input() multiple?: boolean;
+
+    /** use the material style */
+    @Input() material?: string;
+
+    @Output() update = new EventEmitter();
+    @Output() open = new EventEmitter();
+    @Output() search = new EventEmitter();
 
     hoveringValue: common.Select2Value | null | undefined = null;
     option: common.Select2Option | common.Select2Option[] | null = null;
     isOpen = false;
-    focusoutTimer?: number;
+    focusoutTimer?: any;
     innerSearchText = "";
     lastScrollTopIndex = 0;
     isSearchboxHidden: boolean;
@@ -41,10 +51,13 @@ export class Select2Component {
     searchInputElement: HTMLElement;
     resultsElement: HTMLElement;
 
-    @ViewChild("searchInput")
-    searchInput: ElementRef;
-    @ViewChild("results")
-    results: ElementRef;
+    @ViewChild("searchInput") searchInput: ElementRef;
+    @ViewChild("results") results: ElementRef;
+
+    _stateChanges = new Subject<void>();
+
+    /** Whether the element is focused or not. */
+    focused = false;
 
     get searchText() {
         return this.innerSearchText;
@@ -69,7 +82,12 @@ export class Select2Component {
             this.hoveringValue = common.getFirstAvailableOption(result);
 
             if (this.resultsElement) {
-                const lastScrollTopIndex = common.getLastScrollTopIndex(this.hoveringValue, this.resultsElement, result, this.lastScrollTopIndex);
+                const lastScrollTopIndex = common.getLastScrollTopIndex(
+                    this.hoveringValue,
+                    this.resultsElement,
+                    result,
+                    this.lastScrollTopIndex
+                );
                 if (lastScrollTopIndex !== null) {
                     this.lastScrollTopIndex = lastScrollTopIndex;
                 }
@@ -79,15 +97,76 @@ export class Select2Component {
     }
 
     get containerStyle() {
-        return common.getContainerStyle(this.disabled, this.isOpen);
+        return common.getContainerStyle(this.disabled, this.isOpen)
+            + (this.material === '' || this.material === 'true' ? ' material' : '');
     }
 
     get selectionStyle() {
         return common.getSelectionStyle(this.multiple);
     }
 
+    /** Unique id of the element. */
+    @Input()
+    get id() { return this._id; }
+    set id(value: string) { console.log(value); this._id = value || this._uid; }
+
+    /** Whether the element is required. */
+    @Input()
+    get required() { return this._required; }
+    set required(value: any) { this._required = this._coerceBooleanProperty(value); }
+
+    /** Whether the element is disabled. */
+    @Input()
+    get disabled() { return false; /*this._ngControl ? this._ngControl.disabled : this._disabled;*/ }
+    set disabled(value: any) { this._disabled = this._coerceBooleanProperty(value); }
+
+    /** Whether the element is readonly. */
+    @Input()
+    get readonly() { return this._readonly; }
+    set readonly(value: any) { this._readonly = this._coerceBooleanProperty(value); }
+
+    /** The input element's value. */
+    @Input()
+    get value() { return this._value; }
+    set value(value: common.Select2UpdateValue) {
+        this.writeValue(value);
+        this._value = value;
+    }
+
+    /** onTouch function registered via registerOnTouch (ControlValueAccessor). */
+    onTouched: () => any = () => { };
+
+    private _disabled = false;
+    private _required = false;
+    private _readonly = false;
+    private _id: string;
+    private _uid = `select2-${nextUniqueId++}`;
+    private _value: common.Select2UpdateValue;
+    private _previousNativeValue = this._value;
+
+    private _controlValueAccessorChangeFn: (value: any) => void = () => { };
+
+    constructor(
+        private _changeDetectorRef: ChangeDetectorRef,
+        @Optional() private _parentForm: NgForm,
+        @Optional() private _parentFormGroup: FormGroupDirective,
+        @Self() @Optional() public _control: NgControl
+    ) {
+
+        this.id = this.id;
+
+        if (this._control) {
+            this._control.valueAccessor = this;
+        }
+
+    }
+
     ngOnInit() {
-        const option = common.getOptionsByValue(this.data, this.value, this.multiple);
+        const option = common.getOptionsByValue(
+            this.data,
+            this._control ? this._control.value : this.value,
+            this.multiple
+        );
         if (option !== null) {
             this.option = option;
         }
@@ -105,14 +184,20 @@ export class Select2Component {
         this.resultsElement = this.results.nativeElement as HTMLElement;
     }
 
+    ngDoCheck() {
+        this._dirtyCheckNativeValue();
+    }
+
     getOptionStyle(value: string) {
         return common.getOptionStyle(value, this.hoveringValue);
     }
+
     mouseenter(option: common.Select2Option) {
         if (!option.disabled) {
             this.hoveringValue = option.value;
         }
     }
+
     click(option: common.Select2Option) {
         if (!option.disabled) {
             this.select(option);
@@ -121,6 +206,7 @@ export class Select2Component {
             clearTimeout(this.focusoutTimer);
         }
     }
+
     toggleOpenAndClose() {
         if (this.disabled) {
             return;
@@ -139,7 +225,12 @@ export class Select2Component {
             }
 
             if (this.resultsElement) {
-                const lastScrollTopIndex = common.getLastScrollTopIndex(this.hoveringValue, this.resultsElement, this.data, this.lastScrollTopIndex);
+                const lastScrollTopIndex = common.getLastScrollTopIndex(
+                    this.hoveringValue,
+                    this.resultsElement,
+                    this.data,
+                    this.lastScrollTopIndex
+                );
                 if (lastScrollTopIndex !== null) {
                     this.lastScrollTopIndex = lastScrollTopIndex;
                 }
@@ -151,38 +242,53 @@ export class Select2Component {
             clearTimeout(this.focusoutTimer);
         }
     }
+
     focusout() {
         this.focusoutTimer = setTimeout(() => {
             this.isOpen = false;
             this.focusoutTimer = undefined;
         }, common.timeout);
     }
+
     moveUp() {
         this.hoveringValue = common.getPreviousOption(this.filteredData, this.hoveringValue);
 
         if (this.resultsElement) {
-            const lastScrollTopIndex = common.getLastScrollTopIndex(this.hoveringValue, this.resultsElement, this.filteredData, this.lastScrollTopIndex);
+            const lastScrollTopIndex = common.getLastScrollTopIndex(
+                this.hoveringValue,
+                this.resultsElement,
+                this.filteredData,
+                this.lastScrollTopIndex
+            );
             if (lastScrollTopIndex !== null) {
                 this.lastScrollTopIndex = lastScrollTopIndex;
             }
         }
     }
+
     moveDown() {
         this.hoveringValue = common.getNextOption(this.filteredData, this.hoveringValue);
 
         if (this.resultsElement) {
-            const lastScrollTopIndex = common.getLastScrollTopIndex(this.hoveringValue, this.resultsElement, this.filteredData, this.lastScrollTopIndex);
+            const lastScrollTopIndex = common.getLastScrollTopIndex(
+                this.hoveringValue,
+                this.resultsElement,
+                this.filteredData,
+                this.lastScrollTopIndex
+            );
             if (lastScrollTopIndex !== null) {
                 this.lastScrollTopIndex = lastScrollTopIndex;
             }
         }
     }
+
     selectByEnter() {
         if (this.hoveringValue) {
             const option = common.getOptionByValue(this.data, this.hoveringValue);
             this.select(option);
         }
     }
+
     select(option: common.Select2Option | null) {
         if (option !== null) {
             if (this.multiple) {
@@ -205,7 +311,14 @@ export class Select2Component {
             }
         }
 
-        this.update.emit(this.multiple ? (this.option as common.Select2Option[]).map(op => op.value) : (this.option as common.Select2Option).value);
+        let value = this.multiple
+            ? (this.option as common.Select2Option[]).map(op => op.value)
+            : (this.option as common.Select2Option).value
+
+        this.update.emit(value);
+        if (this._control) {
+            this._control.reset(value);
+        }
     }
 
     keyDown(e: KeyboardEvent) {
@@ -220,9 +333,15 @@ export class Select2Component {
             e.preventDefault();
         }
     }
+
+    searchUpdate(e: Event) {
+        this.searchText = (<HTMLInputElement>e.target).value;
+    }
+
     isSelected(option: common.Select2Option) {
         return common.isSelected(this.option, option, this.multiple);
     }
+
     isDisabled(option: common.Select2Option) {
         return option.disabled ? "true" : "false";
     }
@@ -249,5 +368,77 @@ export class Select2Component {
         if (this.focusoutTimer) {
             clearTimeout(this.focusoutTimer);
         }
+    }
+
+    /**
+   * Sets the model value. Implemented as part of ControlValueAccessor.
+   * @param value
+   */
+    writeValue(value: any) {
+        this._setSelectionByValue(value);
+    }
+    /**
+     * Registers a callback to eb triggered when the value has changed.
+     * Implemented as part of ControlValueAccessor.
+     * @param fn Callback to be registered.
+     */
+    registerOnChange(fn: (value: any) => void) {
+        this._controlValueAccessorChangeFn = fn;
+    }
+
+    /**
+     * Registers a callback to be triggered when the component is touched.
+     * Implemented as part of ControlValueAccessor.
+     * @param fn Callback to be registered.
+     */
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
+    }
+
+    /**
+   * Sets whether the component should be disabled.
+   * Implemented as part of ControlValueAccessor.
+   * @param isDisabled
+   */
+    setDisabledState(isDisabled: boolean) {
+        this.disabled = isDisabled;
+    }
+
+    _isErrorState(): boolean {
+        const isInvalid = this._control && this._control.invalid;
+        const isTouched = this._control && this._control.touched;
+        const isSubmitted = (this._parentFormGroup && this._parentFormGroup.submitted) ||
+            (this._parentForm && this._parentForm.submitted);
+
+        return !!(isInvalid && (isTouched || isSubmitted));
+    }
+
+    /**
+   * Sets the selected option based on a value. If no option can be
+   * found with the designated value, the select trigger is cleared.
+   */
+    private _setSelectionByValue(value: any | any[], isUserInput = false): void {
+        const isArray = Array.isArray(value);
+
+        if (this.multiple && value && !isArray) {
+            throw 'Non array value.';
+        }
+
+        this._changeDetectorRef.markForCheck();
+    }
+
+
+    /** Does some manual dirty checking on the native input `value` property. */
+    private _dirtyCheckNativeValue() {
+        const newValue = this.value;
+
+        if (this._previousNativeValue !== newValue) {
+            this._previousNativeValue = newValue;
+            this._stateChanges.next();
+        }
+    }
+
+    private _coerceBooleanProperty(value: any): boolean {
+        return value != null && `${value}` !== 'false';
     }
 }
