974ade55ec1b6e75a8eebd79c983390224696562
york yao <yaoao12306@outlook.com>
2017-08-02 18:56:39 +0800

    update webpack config

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -10,7 +10,7 @@ module.exports = {
     `lessc src/select2.less > dist/select2.css`,
     `cleancss -o dist/select2.min.css dist/select2.css`,
     `cleancss -o demo/index.bundle.css dist/select2.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --config demo/webpack.config.js`,
+    `webpack --display-modules --config demo/webpack.config.js`,
     `rev-static --config demo/rev-static.config.js`
   ],
   lint: [
