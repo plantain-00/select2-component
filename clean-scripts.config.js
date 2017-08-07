25a2a46fd3c3b79c718b514dfe6b97e7a5cf2817
york yao <yaoao12306@outlook.com>
2017-08-07 19:08:10 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,31 +1,37 @@
 module.exports = {
   build: [
     `rimraf dist`,
-    `rimraf demo/**/index.bundle-*.js`,
-    `rimraf demo/*.bundle-*.css`,
-    `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
-    `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src`,
-    `ngc -p src`,
-    `tsc -p demo`,
-    `lessc src/select2.less > dist/select2.css`,
-    `cleancss -o dist/select2.min.css dist/select2.css`,
-    `cleancss -o demo/index.bundle.css dist/select2.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --display-modules --config demo/webpack.config.js`,
+    {
+      js: [
+        {
+          vue: `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
+          angular: `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src`
+        },
+        `ngc -p src`,
+        `tsc -p demo`,
+        `webpack --display-modules --config demo/webpack.config.js`
+      ],
+      css: [
+        `lessc src/select2.less > dist/select2.css`,
+        `cleancss -o dist/select2.min.css dist/select2.css`,
+        `cleancss -o demo/index.bundle.css dist/select2.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+      ],
+      clean: [
+        `rimraf demo/**/index.bundle-*.js`,
+        `rimraf demo/*.bundle-*.css`
+      ]
+    },
     `rev-static --config demo/rev-static.config.js`
   ],
-  lint: [
-    `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
-    `standard "**/*.config.js"`,
-    `stylelint "src/**/*.less"`
-  ],
+  lint: {
+    ts: `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "src/**/*.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  fix: `standard --fix "**/*.config.js"`,
+  release: `clean-release`
 }
