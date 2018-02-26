6f100fa4cf38469a22a61a82ef1ae58864cf7782
york yao <yaoao12306@outlook.com>
2018-02-27 07:56:35 +0800

    build: improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -8,19 +8,22 @@ const excludeTsFiles = `"packages/@(core|vue|react|angular)/@(src|demo)/**/*.d.t
 
 const vueTemplateCommand = `file2variable-cli --config packages/vue/src/file2variable.config.js`
 const angularTemplateCommand = `file2variable-cli packages/angular/src/index.template.html -o packages/angular/src/variables.ts --html-minify --base packages/angular/src`
-const ngcSrcCommand = [
-  `ngc -p packages/core/src`,
-  `tsc -p packages/vue/src`,
-  `tsc -p packages/react/src`,
-  `ngc -p packages/angular/src`
-]
-const tscDemoCommand = [
-  `ngc -p packages/core/demo`,
-  `tsc -p packages/vue/demo`,
-  `tsc -p packages/react/demo`,
-  `ngc -p packages/angular/demo`
-]
-const webpackCommand = `webpack`
+
+const tscCoreSrcCommand = `ngc -p packages/core/src`
+const tscVueSrcCommand = `tsc -p packages/vue/src`
+const tscReactSrcCommand = `tsc -p packages/react/src`
+const tscAngularSrcCommand = `ngc -p packages/angular/src`
+
+const tscCoreDemoCommand = `ngc -p packages/core/demo`
+const tscVueDemoCommand = `tsc -p packages/vue/demo`
+const tscReactDemoCommand = `tsc -p packages/react/demo`
+const tscAngularDemoCommand = `ngc -p packages/angular/demo`
+
+const webpackVueCommand = `webpack --config packages/vue/demo/webpack.config.js`
+const webpackReactCommand = `webpack --config packages/react/demo/webpack.config.js`
+const webpackAngularJitCommand = `webpack --config packages/angular/demo/jit/webpack.config.js`
+const webpackAngularAotCommand = `webpack --config packages/angular/demo/aot/webpack.config.js`
+
 const revStaticCommand = `rev-static`
 const cssCommand = [
   `lessc packages/core/src/select2.less -sm=on > packages/core/src/select2.css`,
@@ -33,13 +36,30 @@ module.exports = {
   build: [
     {
       js: [
+        tscCoreSrcCommand,
+        tscCoreDemoCommand,
         {
-          vue: vueTemplateCommand,
-          angular: angularTemplateCommand
-        },
-        ngcSrcCommand,
-        tscDemoCommand,
-        webpackCommand
+          vue: [
+            vueTemplateCommand,
+            tscVueSrcCommand,
+            tscVueDemoCommand,
+            webpackVueCommand
+          ],
+          react: [
+            tscReactSrcCommand,
+            tscReactDemoCommand,
+            webpackReactCommand
+          ],
+          angular: [
+            angularTemplateCommand,
+            tscAngularSrcCommand,
+            tscAngularDemoCommand,
+            {
+              webpackAngularJitCommand,
+              webpackAngularAotCommand
+            }
+          ]
+        }
       ],
       css: cssCommand,
       clean: `rimraf "packages/@(core|vue|react|angular)/demo/**/@(*.bundle-*.js|*.bundle-*.css)"`
@@ -65,11 +85,20 @@ module.exports = {
     less: `stylelint --fix ${lessFiles}`
   },
   watch: {
-    vue: `${vueTemplateCommand} --watch`,
-    angular: `${angularTemplateCommand} --watch`,
-    src: `${ngcSrcCommand} --watch`,
-    demo: `${tscDemoCommand} --watch`,
-    webpack: `${webpackCommand} --watch`,
+    vueTemplateCommand: `${vueTemplateCommand} --watch`,
+    angularTemplateCommand: `${angularTemplateCommand} --watch`,
+    tscCoreSrcCommand: `${tscCoreSrcCommand} --watch`,
+    tscVueSrcCommand: `${tscVueSrcCommand} --watch`,
+    tscReactSrcCommand: `${tscReactSrcCommand} --watch`,
+    tscAngularSrcCommand: `${tscAngularSrcCommand} --watch`,
+    tscCoreDemoCommand: `${tscCoreDemoCommand} --watch`,
+    tscVueDemoCommand: `${tscVueDemoCommand} --watch`,
+    tscReactDemoCommand: `${tscReactDemoCommand} --watch`,
+    tscAngularDemoCommand: `${tscAngularDemoCommand} --watch`,
+    webpackVueCommand: `${webpackVueCommand} --watch`,
+    webpackReactCommand: `${webpackReactCommand} --watch`,
+    webpackAngularJitCommand: `${webpackAngularJitCommand} --watch`,
+    webpackAngularAotCommand: `${webpackAngularAotCommand} --watch`,
     less: () => watch(['src/**/*.less'], [], () => executeScriptAsync(cssCommand)),
     rev: `${revStaticCommand} --watch`
   },
