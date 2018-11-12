f10555fecf4c25a5207e3c3e611f3dbccac8aaba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-12 12:51:44 +0000

    Deploy plantain-00/select2-component to github.com/plantain-00/select2-component.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -77,7 +77,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude ${excludeTsFiles}`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'lerna exec -- type-coverage -p src --at-least 89'
+    typeCoverage: 'lerna exec -- type-coverage -p src'
   },
   test: [
     'tsc -p spec',
